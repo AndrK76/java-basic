1377d113fd45a11bdcea821bb4d2e673773ca66f
AndrK76 <andrk-zlt@74.ru>
2022-12-13 19:40:22 +0500

    HW Complete

--- lesson06/ru/otus/java/hw/MiniTester.java
+++ lesson06/ru/otus/java/hw/MiniTester.java
@@ -3,43 +3,48 @@ package ru.otus.java.hw;
 import java.util.Scanner;
 
 /**
- * Заготовка для выполнения ДЗ "Система тестирования".
+ * ДЗ "Система тестирования". Итерация 1
  */
 public class MiniTester {
     public static void main(String[] args) {
         // Переменные для хранения количества правильных и неправильных ответов
         int correctCount = 0, wrongCount = 0;
 
-        // Ниже вместо null надо написать реализацию,
-        // написано так, чтобы просто компилировалось
-
-        // TODO: Массив вопросов (вместо null надо написать определение массива)
-        String[] questions = null;
-
-        // TODO: Массив вариантов ответов
-        String[][] answerOptions = null;
-
-        // TODO: Массив правильных ответов
-        int[] correctAnswers = null;
-
-        // Примечание - можете придумать как хранить всю информацию в одном массиве
+        //Массив вопросов
+        String[] questions = {
+                "В файл с каким расширением компилируется java-файл?",
+                "С помощью какой команды git можно получить полную копию удаленного репозитория?",
+                "Какой применяется цикл, когда не известно количество итераций?",
+        };
+        //Массив вариантов ответов
+        String[][] answerOptions = {
+                {"cs", "java", "class", "exe"},
+                {"commit", "push", "clone", "copy"},
+                {"while", "for", "loop"},
+        };
+        //Массив правильных ответов
+        int[] correctAnswers = {2, 2, 0};
 
         Scanner scanner = new Scanner(System.in);
-        // TODO: Цикл по всем вопросам - исправить, написать правильно
-        for(int i = 0; i < 0; i++) {
-            // TODO: Вывод вопроса на экран
-
-            // TODO: Вывод вариантов ответов на экран
-
+        //Цикл по всем вопросам
+        for (int i = 0; i < questions.length; i++) {
+            System.out.println("Вопрос №" + (i + 1) + ":");
+            System.out.println(questions[i]);
+            for (int j = 0; j < answerOptions[i].length; j++) {
+                System.out.println((j + 1) + ". " + answerOptions[i][j]);
+            }
             System.out.print("Ваш ответ: ");
-            //TODO: Считываем с консоли ответ пользователя
-
-            //TODO: Проверяем ответ и выводим результат
-            // а также увеличиваем счетчики правильных и неправильных ответов
-
+            int answer = scanner.nextInt();
+            if (answer == correctAnswers[i]+1){
+                System.out.println("Правильно");
+                correctCount++;
+            }
+            else {
+                System.out.println("Неправильно");
+                wrongCount++;
+            }
             System.out.println();
         }
-
         //Выводим общий результат
         System.out.println("Результат: правильно " + correctCount + ", неправильно " + wrongCount);
     }

0544c5aeabfc7c6925b4b51656482cb5bbe2fb8f
AndrK76 <andrk-zlt@74.ru>
2022-12-22 16:42:36 +0500

    Assert T modidied

--- lesson16/src/main/java/ru/otus/assertions/Assertions.java
+++ lesson16/src/main/java/ru/otus/assertions/Assertions.java
@@ -2,6 +2,9 @@ package ru.otus.assertions;
 
 import ru.otus.game.Player;
 
+import java.lang.reflect.InvocationTargetException;
+import java.util.Optional;
+
 public class Assertions {
     public static void assertEquals(boolean expected, boolean actual) {
         if (expected != actual) {
@@ -28,13 +31,29 @@ public class Assertions {
     }
 
     public static <T> void assertEquals(T expected, T actual) {
-        if (expected == null && actual == null) {
-            return;
+        if (!Optional.ofNullable(expected).equals(Optional.ofNullable(actual))) {
+            throw new AssertionError(String.format("Expected \"%s\" = \"%s\"", getNameObject(expected), getNameObject(actual)));
         }
-        if (expected == null || actual == null || !expected.equals(actual)) {
-            throw new AssertionError(String.format("Expected \"%s\" = \"%s\"", expected, actual));
+//        if (expected == null && actual == null) {
+//            return;
+//        }
+//        if (expected == null || actual == null || !expected.equals(actual)) {
+//            throw new AssertionError(String.format("Expected \"%s\" = \"%s\"", expected, actual));
+//        }
+    }
+
+    static <T> String getNameObject(T value) {
+        if (value == null) {
+            return "null";
+        }
+        try {
+            var getName = value.getClass().getMethod("getName");
+            return getName.invoke(value).toString();
+        } catch (Throwable e) {
+            return value.toString();
         }
     }
+
 
     public static void assertThrows(Class<? extends Throwable> expected, Runnable code) {
         Throwable actual = null;
